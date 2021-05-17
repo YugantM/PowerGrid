656c175f83e8f961a9c780fe3bd693bdbadd51a7
Yugant M. Hadiyal <hadiyalyugant@gmail.com>
2021-05-17 18:50:13 +0200

    classifier added

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,23 +1,31 @@
-#this file contains code for FLASK app connected with MySQL
+ #this file contains code for FLASK app connected with MySQL
 
 ## Import start
 from flask import Flask,render_template,url_for,request,redirect
 import mysql.connector
 import re
 import pandas as pd
+import matlab.engine
+from datetime import datetime
 #---Import end---
 
-quoted = re.compile('"[^"]*"')
 
+# strating matlab
+print("Starting matlab...")
+eng = matlab.engine.start_matlab()
+
+# slx for simulink files
+simulation_list = eng.what('/Users/yuganthadiyal/Documents/MATLAB')['slx']
 
 config = {
   'host':"localhost",
   'user':"root",
   'password':"#Include76002",
-  'database' : 'mysql'
+  'database' : 'simulation'
 }
 
 
+print("Connecting with database...")
 mydb = mysql.connector.connect(**config)
 
 mycursor = mydb.cursor()
@@ -29,23 +37,36 @@ app = Flask(__name__)
 
 @app.route("/", methods = ['GET'])
 def home():
-	global tables,mycursor
+	global tables,mycursor,simulation_list
 
 	mycursor.execute("SHOW TABLES;")
 	tables = [x[0] for x in mycursor.fetchall()]
 
 	
-	return render_template('index.html',simulation_list=tables)
+	return render_template('index.html',tables=tables,simulation_list=simulation_list)
 
 @app.route("/selected_item",methods=['POST'])
 def selected_item():
 
+	global tables,mycursor,simulation_list,selected_value
+
 	meta = {}
+	dummy = {"a":"a","b":"b"}
 
-	table_name = request.form['selected']
-	print(request.form['selected'])
+	selected_value = request.form['selected']
 
-	df = pd.read_sql("select * from "+str(table_name)+";", mydb)
+	if len(tables)>0:
+		if selected_value in tables:
+			table_name = selected_value
+			df = pd.read_sql("select * from "+str(table_name)+";", mydb)
+		elif selected_value in simulation_list:
+			df = pd.DataFrame(dummy,index=[0])
+
+	else:
+		df = pd.DataFrame(dummy,index=[0])
+
+	
+
 
 	meta['Dimensions'] = str(df.shape[0])+" x "+str(df.shape[1])+" (rows x columns)" + ", total: "+ str(df.shape[0]*df.shape[1])+" (cells)"
 	meta['Columns'] = ",".join(list(df.columns))
@@ -58,10 +79,14 @@ def selected_item():
 
 	des = df.describe().to_html()
 
-	return render_template('data.html',title=request.form['selected'],lista = temp,data = html,meta=meta,describe=des)
+	return render_template('data.html',title=selected_value,lista = temp,data = html,meta=meta,describe=des)
 
 @app.route("/selected_simulation",methods=['POST'])
 def selected_simulation():
+
+	global selected_value,mycursor,eng
+
+
 
 	if request.form['selected'] == 'back':
 
@@ -69,6 +94,9 @@ def selected_simulation():
 
 	elif request.form['selected'] == 'run':
 
+		stamp = "_"+datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+
+		eng.sim(selected_value)
 		# code to run the simulation named "request.form['selected']"
 		return "simulation running"
 	else:
@@ -97,7 +125,7 @@ def config_response():
 	global mydb,mycursor,tables
 
 	if request.form['selected']=='cancel':
-		return home()
+		return home() 
 
 	name = request.form['database']
 	print(name)
