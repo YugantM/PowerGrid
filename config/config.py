0231ea19fc6628c7899c16fcf59d2dd821dc3246
Yugant M. Hadiyal <hadiyalyugant@gmail.com>
2021-05-26 21:57:42 +0200

    validated models

---
+++ config/config.py
@@ -0,0 +1,3 @@
+binary_batch_size = 20000
+multi_batch_size = 2000
+test_file = 'ml_test.csv'
---
+++ config/constants.py
@@ -0,0 +1,3 @@
+col_len = 6
+column_names = ['A'+str(each+1) for each in range(int(col_len/2))] + ['V'+str(each+1) for each in range(int(col_len/2))]
+all_labels = ['NML','AB', 'AC', 'BC', 'ABC', 'AG', 'BG', 'ABG', 'CG', 'ACG', 'BCG', 'ABCG']
